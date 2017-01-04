e63a5900a0c50be6001f389fce12ab1c2b00ca66
Rostyslav Khanas <rtkhanas@gmail.com>
2017-01-04 20:36:26 +0900

    Fix svg even propagation

--- packages/binary-ui-icons/src/components-styled/SurfaceWrapper/index.js
+++ packages/binary-ui-icons/src/components-styled/SurfaceWrapper/index.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export default styled.div`
   display: inline-block;
   height: ${props => props.size}px;
+  pointer-events: visibleStroke;
   vertical-align: middle;
   width: ${props => props.size}px;
 `;
