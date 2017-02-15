af08a5106ff9f6edcca2109f7b594cd97da18213
Ross Khanas <rtkhanas@users.noreply.github.com>
2017-02-15 11:19:13 +0900

    Update index.jsx

--- packages/binary-ui-components/src/components/ActionIcon/components/index.jsx
+++ packages/binary-ui-components/src/components/ActionIcon/components/index.jsx
@@ -1,6 +1,7 @@
 import autobind from 'autobind-decorator';
 import React from 'react';
 import ActionableIcon from '../../ActionableIcon';
+import { isLeftButton } from '../../../utils/events';
 
 const propTypes = {
   color: React.PropTypes.string,
@@ -65,7 +66,7 @@ export default class ActionIcon extends React.Component {
         isActive={isActive}
         isHover={isHover}
         isDisabled={isDisabled}
-        onMouseDown={!isDisabled && ((e) => { this.onSetActive(true); if (onTapDown) { onTapDown(e); } })}
+        onMouseDown={!isDisabled && ((e) => { if (isLeftButton(e)) { this.onSetActive(true); } if (onTapDown) { onTapDown(e); } })}
         onTouchStart={!isDisabled && ((e) => { this.onSetActive(true); if (onTapDown) { onTapDown(e); } })}
         onMouseEnter={!isDisabled && ((e) => { this.onSetHover(true); if (onMouseEnter) { onMouseEnter(e); } })}
         onMouseLeave={!isDisabled && ((e) => { this.onSetHover(false); if (onMouseLeave) { onMouseLeave(e); } })}
