bc9d5d0c4604926793f665600e9ddc7d376fb31b
Ross Khanas <rtkhanas@users.noreply.github.com>
2017-02-22 10:36:14 +0900

    Update SliderScaleIcon.js

--- packages/binary-ui-components/src/components/Slider/components-styled/SliderScaleIcon.js
+++ packages/binary-ui-components/src/components/Slider/components-styled/SliderScaleIcon.js
@@ -1,8 +1,8 @@
-import { LIST_ITEM_HALF_HEIGHT } from '../../../utils/styles.universal';
 import styled from 'styled-components';
-import { ActionIcon } from 'binary-ui-components';
+import ActionIcon from '../../ActionIcon';
+import { LIST_ITEM_HALF_HEIGHT } from '../../../utils/styles.universal';
 
-export default styled.ActionIcon`
+export default styled(ActionIcon)`
   height: ${LIST_ITEM_HALF_HEIGHT}px;
   width: ${LIST_ITEM_HALF_HEIGHT}px;
   margin: 10px;
