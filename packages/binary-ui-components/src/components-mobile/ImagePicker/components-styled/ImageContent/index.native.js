ac6f61caefe79f0d59271c1a73bc3fea6193d70c
Igor Dzhebyan <id@try.cards>
2017-08-28 14:32:40 +0900

    Add border to image in ImagePicker

--- packages/binary-ui-components/src/components-mobile/ImagePicker/components-styled/ImageContent/index.native.js
+++ packages/binary-ui-components/src/components-mobile/ImagePicker/components-styled/ImageContent/index.native.js
@@ -3,6 +3,8 @@ import { PHOTO_SIZE } from '../../utils/styles.native-and-web';
 
 export default styled.Image`
   border-radius: ${PHOTO_SIZE / 2}px;
+  border-width: 1px;
+  border-color: #aeaeae;
   height: ${PHOTO_SIZE}px;
   margin: 5px;
   width: ${PHOTO_SIZE}px;
