40348ce65a244db9006d42e7973a81e8e6046270
Rostyslav Khanas <rtkhanas@gmail.com>
2017-09-24 08:12:05 +0300

    Fix image border radius

--- packages/binary-ui-components/src/components-mobile/ImagePicker/components-styled/ImageContent/index.native.js
+++ packages/binary-ui-components/src/components-mobile/ImagePicker/components-styled/ImageContent/index.native.js
@@ -2,8 +2,9 @@ import styled from 'styled-components/native';
 import { PHOTO_SIZE } from '../../utils/styles.native-and-web';
 
 export default styled.Image`
-  border-width: 1px;
   border-color: #aeaeae;
+  border-radius: ${PHOTO_SIZE / 2}px;
+  border-width: 1px;
   height: ${PHOTO_SIZE}px;
   margin: 5px;
   width: ${PHOTO_SIZE}px;
