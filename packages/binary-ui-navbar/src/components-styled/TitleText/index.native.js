e07572ce9d720b72fb0dbddfdcf87a61befcf773
Rostyslav Khanas <rtkhanas@gmail.com>
2018-04-02 18:30:05 +0300

    Nav bar is bold

--- packages/binary-ui-navbar/src/components-styled/TitleText/index.native.js
+++ packages/binary-ui-navbar/src/components-styled/TitleText/index.native.js
@@ -7,8 +7,8 @@ export default styled.Text.attrs({
   align-items: center;
   color: #000000;
   flex-direction: row;
-  font-family: ${props => props.theme.fontFamily500};
+  font-family: ${props => props.theme.fontFamily600};
   font-size: 16px;
-  font-weight: ${props => props.theme.fontWeight500};
+  font-weight: ${props => props.theme.fontWeight600};
   justify-content: flex-start;
 `;
