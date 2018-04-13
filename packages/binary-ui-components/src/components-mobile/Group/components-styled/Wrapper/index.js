de978d2774a40fc0868eda37f6be782fb2e4c1a3
Rostyslav Khanas <rtkhanas@gmail.com>
2018-04-13 14:41:11 +0300

    Remove prevent selection in section and group

--- packages/binary-ui-components/src/components-mobile/Group/components-styled/Wrapper/index.js
+++ packages/binary-ui-components/src/components-mobile/Group/components-styled/Wrapper/index.js
@@ -5,7 +5,6 @@ export default styled.div`
   display: flex;
   height: ${props => props.theme.sizeListItemHeight}px;
   max-width: 100%;
-  user-select: none;
   width: 100%;
   -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
   -webkit-touch-callout: none;
--- packages/binary-ui-components/src/components-mobile/Section/components-styled/SectionWrapper/index.js
+++ packages/binary-ui-components/src/components-mobile/Section/components-styled/SectionWrapper/index.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 export default styled.div`
-  user-select: none;
   -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
   -webkit-touch-callout: none;
 `;
