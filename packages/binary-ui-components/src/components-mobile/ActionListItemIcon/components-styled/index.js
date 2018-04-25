48ccc3cf471e1d5411c57ca1618ec1d9ef586876
Rostyslav Khanas <rtkhanas@gmail.com>
2018-04-25 20:23:01 +0300

    v0.6.7

--- packages/binary-ui-components/src/components-mobile/ActionListItemIcon/components-styled/index.js
+++ packages/binary-ui-components/src/components-mobile/ActionListItemIcon/components-styled/index.js
@@ -2,5 +2,5 @@ import styled from 'styled-components';
 import Index from '../components';
 
 export default styled(Index)`
-  padding: 4px;
+  padding: 4px 4px 3px 4px;
 `;
--- packages/binary-ui-components/src/components-mobile/ActionListItemIcon/components-styled/index.native.js
+++ packages/binary-ui-components/src/components-mobile/ActionListItemIcon/components-styled/index.native.js
@@ -3,5 +3,5 @@ import Index from '../components';
 
 export default styled(Index)`
   justify-content: center;
-  padding: 4px;
+  padding: 4px 4px 3px 4px;
 `;
