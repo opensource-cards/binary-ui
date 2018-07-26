b1766b6496194b752c5eba5865e09c8ebd1b929e
Rostyslav Khanas <rtkhanas@gmail.com>
2018-07-26 10:56:33 +0300

    Fix styles

--- packages/binary-ui-components/src/Switch/components-styled/SwitchContainer.js
+++ packages/binary-ui-components/src/Switch/components-styled/SwitchContainer.js
@@ -3,4 +3,5 @@ import styled from 'styled-components';
 export default styled.div`
   height: ${props => props.theme.sizeListItemHeight / 2}px;
   position: relative;
+  width: ${props => props.theme.sizeListItemHeight}px;
 `;
--- packages/binary-ui-components/src/Switch/components-styled/SwitchLabel/index.js
+++ packages/binary-ui-components/src/Switch/components-styled/SwitchLabel/index.js
@@ -2,5 +2,5 @@ import styled from 'styled-components';
 import Label from '../../../Label';
 
 export default styled(Label)`
-  width: calc(100% - ${props => props.theme.sizeListItemHeight})%;
+  width: calc(100% - ${props => props.theme.sizeListItemHeight}px);
 `;
