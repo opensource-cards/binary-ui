5bc7b891afcb302ffcb68a26b88dfc1741214fe1
Rostyslav Khanas <rtkhanas@gmail.com>
2018-08-27 22:29:53 +0300

    Switch cursor style

--- packages/binary-ui-components/src/Switch/components-styled/SwitchToggle.js
+++ packages/binary-ui-components/src/Switch/components-styled/SwitchToggle.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export default styled.div`
+  cursor: ${props => (props.isDisabled ? 'default' : 'pointer')};
   opacity: ${props => (props.isDisabled ? props.theme.opacityDisabled : 1)};
   margin-right: 10px;
   width: ${props => props.theme.sizeListItemHeight}px;
--- packages/binary-ui-components/src/Switch/components-styled/SwitchWrapper/index.js
+++ packages/binary-ui-components/src/Switch/components-styled/SwitchWrapper/index.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export default styled.div`
   align-items: center;
   background-color: #ffffff;
-  cursor: ${props => (props.isDisabled ? 'default' : 'pointer')};
   display: flex;
   flex-direction: row;
   height: ${props => props.theme.sizeListItemHeight}px;
