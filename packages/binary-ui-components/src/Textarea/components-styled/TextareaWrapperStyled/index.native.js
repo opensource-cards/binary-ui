b9d8ab3bfbaf714fc9563e50bf6d701ded697ebe
Rostyslav Khanas <rtkhanas@gmail.com>
2018-09-04 15:06:07 +0300

    Ref

--- packages/binary-ui-components/src/Textarea/components-styled/TextareaWrapperStyled/index.native.js
+++ packages/binary-ui-components/src/Textarea/components-styled/TextareaWrapperStyled/index.native.js
@@ -2,6 +2,7 @@ import { getHighlightEditStyle } from 'binary-ui-styles';
 import styled from 'styled-components';
 
 export default styled.View`
+  background-color: #ffffff;
   justify-content: space-between;
   flex-direction: row;
   ${props => (
